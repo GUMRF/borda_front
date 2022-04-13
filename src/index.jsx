648341afe5733fa06c8baae5138d5249fe2f1ab8
Max <2thousandmax@gmail.com>
2022-04-13 12:44:17 +0300

    Add sign in form

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store'
 
@@ -10,9 +10,9 @@ import { Home } from './pages/home';
 import { Challenges } from './pages/challenges';
 import { Scoreboard } from './pages/scoreboard';
 import { FAQ } from './pages/faq';
-import {SignIn} from './pages/sign-in'
-import {Registration} from './pages/registration'
-import {SignOut} from './pages/sign-out'
+import { SignIn } from './pages/sign-in'
+import { Registration } from './pages/registration'
+import { SignOut } from './pages/sign-out'
 
 import Header from './components/Header';
 
@@ -22,7 +22,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-         <App />
+        <App />
       </Router>
     </Provider>
   </React.StrictMode>,
@@ -34,16 +34,16 @@ function App() {
   let location = useLocation();
   return (
     <>
-        {location.pathname !== "/" ? (<Header />) : null}
-        <Routes>
-          <Route exact path="/" element={< Home />} />
-          <Route exact path="/challenges" element={< Challenges />} />
-          <Route exact path="/scoreboard" element={< Scoreboard />} />
-          <Route exact path="/faq" element={< FAQ />} />
-          <Route exact path="/sign-in" element={< SignIn />} />
-          <Route exact path="/register" element={< Registration />} />
-          <Route exact path="/sign-out" element={<SignOut/> }  />
-        </Routes>
+      {location.pathname !== "/sign-in" ? (<Header />) : null}
+      <Routes>
+        <Route exact path="/" element={< Home />} />
+        <Route exact path="/challenges" element={< Challenges />} />
+        <Route exact path="/scoreboard" element={< Scoreboard />} />
+        <Route exact path="/faq" element={< FAQ />} />
+        <Route exact path="/sign-in" element={< SignIn />} />
+        <Route exact path="/register" element={< Registration />} />
+        <Route exact path="/sign-out" element={<SignOut />} />
+      </Routes>
 
     </>
   );
--- src/pages/sign-in.jsx
+++ src/pages/sign-in.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { LockClosedIcon } from '@heroicons/react/solid'
+
 import { useGetAuthMutation } from "../api/auth";
 
 export function SignIn() {
   const [newLogin, setNewLogin] = useState("");
   const [newPass, setNewPass] = useState("");
-  const [inputLogin, {}] = useGetAuthMutation();
+  const [inputLogin, { }] = useGetAuthMutation();
 
   const handleLogin = async () => {
     if (newLogin) {
@@ -18,25 +20,64 @@ export function SignIn() {
 
   return (
     <>
-      <div>
-        <input
-          type="text"
-          value={newLogin}
-          onChange={(e) => setNewLogin(e.target.value)}
-        />
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+          </div>
+          <form className="mt-8 space-y-6">
+            <input type="hidden" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="username" className="sr-only">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  value={newLogin}
+                  onChange={(e) => setNewLogin(e.target.value)}
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Username"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  value={newPass}
+                  onChange={(e) => setNewPass(e.target.value)}
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="button"
+                onClick={handleLogin}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                </span>
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div>
-        <input
-          type="text"
-          value={newPass}
-          onChange={(e) => setNewPass(e.target.value)}
-        />
-      </div>
-      <button
-        type="button"
-        class="px-5 py-2.5 text-black bg-white focus:ring-4 focus:outline-none focus:ring-grey font-medium rounded-lg text-sm text-center"
-        onClick={handleLogin}
-      ></button>
     </>
   );
 }
